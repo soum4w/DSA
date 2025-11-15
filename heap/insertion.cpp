11b7ed32430d544c61210d06796a5e5c70c6a313
soumaw <soumy18a@gmail.com>
2025-11-15 11:56:09 +0530

    updated file

--- heap/insertion.cpp
+++ heap/insertion.cpp
@@ -11,7 +11,7 @@ class maxHeap{
         size = 0;
         arr[0] = -1;
     }
-
+    //insertion
     void insert(int val){
         size++;
         int index = size;
@@ -27,6 +27,37 @@ class maxHeap{
         }
     }
 
+    //deletion
+    void deleteFromHeap() {
+        if(size == 0) {
+            cout << "Nothing to delete.";
+            return;
+        }
+
+        arr[1] = arr[size];
+        size--;
+
+        int i = 1;
+
+        while(i <= size) {
+            int leftIndex = 2*i;
+            int rightIndex = 2*i + 1;
+            int largest = i;
+
+            if(leftIndex <= size && arr[leftIndex] > arr[largest])
+                largest = leftIndex;
+
+            if(rightIndex <= size && arr[rightIndex] > arr[largest])
+                largest = rightIndex;
+
+            if(largest != i) {
+                swap(arr[i], arr[largest]);
+                i = largest;
+            } else break;
+        }
+    }
+
+
     void print(){
         for(int i=1; i<=size; i++){
             cout<<arr[i]<<" ";
@@ -41,6 +72,12 @@ int main(){
     h.insert(99);
     h.insert(78);
     h.insert(34);
+    h.insert(27);
+    h.insert(91);
+
+    h.print();
+
+    h.deleteFromHeap();
 
     h.print();
     return 0;
