18a88d7a58b867533b98f5a3fe91ad40376c7bab
soumaw <soumy18a@gmail.com>
2025-12-24 19:57:28 +0530

    DNF

--- algorithms/DNF.cpp
+++ algorithms/DNF.cpp
@@ -4,25 +4,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int n= nums.size();
+        int low=0, mid = 0, high = n-1;
 
-        //using Dutch National Flag Algorithm
-        int n = nums.size();
-        int low = 0, mid =0, high = n-1;
         while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums[mid], nums[low]);
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            } else {
-                swap(nums[mid], nums[high]);
-                high--;
-            }
+            if(nums[mid]==0) swap(nums[low++], nums[mid++]);
+            else if(nums[mid]==1) mid++;
+            else swap(nums[mid], nums[high--]);
         }
     }
 };
