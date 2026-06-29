bb209ed4ecf3a1f5c808d603e4b54365622e066f
Soum <soumy18a@gmail.com>
2026-06-30 01:34:56 +0530

    check if the array is sorted & union of two sorted arrays

---
+++ tuf/arrays/UnionOfTwoArr.java
@@ -0,0 +1,80 @@
+package tuf.arrays;
+
+import java.util.ArrayList;
+
+class Solution {
+    public int[] unionArray(int[] nums1, int[] nums2) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) {
+                    ans.add(nums1[i]);
+                }
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                if (ans.isEmpty() || ans.get(ans.size() - 1) != nums2[j]) {
+                    ans.add(nums2[j]);
+                }
+                j++;
+            } else {
+                if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) {
+                    ans.add(nums1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        // add remaining
+        while (i < nums1.length) {
+            if (ans.isEmpty() || ans.get(ans.size() - 1) != nums1[i]) {
+                ans.add(nums1[i]);
+            }
+            i++;
+        }
+        while (j < nums2.length) {
+            if (ans.isEmpty() || ans.get(ans.size() - 1) != nums2[j]) {
+                ans.add(nums2[j]);
+            }
+            j++;
+        }
+        // copy Arraylist to int[]
+        int[] arr = new int[ans.size()];
+        for (int k = 0; k < ans.size(); k++) {
+            arr[k] = ans.get(k);
+        }
+        return arr;
+
+    }
+}
+
+/*
+ * Given two sorted arrays nums1 and nums2, return an array that contains the
+ * union of these two arrays. The elements in the union must be in ascending
+ * order.
+ * 
+ * The union of two arrays is an array where all values are distinct and are
+ * present in either the first array, the second array, or both.
+ * 
+ * Example 1
+ * 
+ * Input: nums1 = [1, 2, 3, 4, 5], nums2 = [1, 2, 7]
+ * 
+ * Output: [1, 2, 3, 4, 5, 7]
+ * 
+ * Explanation:
+ * 
+ * The elements 1, 2 are common to both, 3, 4, 5 are from nums1 and 7 is from
+ * nums2
+ * 
+ * Example 2
+ * 
+ * Input: nums1 = [3, 4, 6, 7, 9, 9], nums2 = [1, 5, 7, 8, 8]
+ * 
+ * Output: [1, 3, 4, 5, 6, 7, 8, 9]
+ * 
+ * Explanation:
+ * 
+ * The element 7 is common to both, 3, 4, 6, 9 are from nums1 and 1, 5, 8 is
+ * from nums2
+ */
