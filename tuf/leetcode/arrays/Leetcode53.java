5c26271d7b1361e540e5a3f6aa6de02cd1f08ecd
Soum <soumy18a@gmail.com>
2026-07-02 23:07:45 +0530

    Kadane's Algorithm

---
+++ tuf/leetcode/arrays/Leetcode53.java
@@ -0,0 +1,13 @@
+package tuf.leetcode.arrays;
+//Kadane's Algorithm
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            currentSum = Math.max(nums[i], nums[i]+currentSum);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+}
